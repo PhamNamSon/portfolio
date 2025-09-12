10236868159842ff3ddf144f3d7bb047cadb5995
PhamNamSon <phamnamson1999@gmail.com>
2025-09-12 13:25:51 -0600

    feat: Add Application Load Balancer (ALB) module with security group and listener configuration

--- infra/EC2_prod/main.tf
+++ infra/EC2_prod/main.tf
@@ -4,3 +4,10 @@ module "vpc" {
   azs                 = var.azs
   public_subnet_cidrs = var.public_subnet_cidrs
 }
+
+module "alb" {
+  source            = "../modules/alb"
+  name              = var.name
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+}
