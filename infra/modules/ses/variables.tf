8d8b4b81a7b7c0921f2490229f29ac3ed21b5a0a
Nam Son <phamnamson1999@gmail.com>
2026-07-20 16:35:07 -0600

    refactor: remove unused SES and Lambda module files

--- infra/modules/ses/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "root_domain" {
-  description = "Root domain you own in Route 53 (e.g., namson.io)"
-  type        = string
-}
-
-variable "region" {
-  description = "AWS region for SES (same as Lambda/API GW)"
-  type        = string
-}
